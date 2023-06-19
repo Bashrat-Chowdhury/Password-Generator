e7d6f537f10fc9911ba39307ba61f2f761798beb
bashratchy <bashrat03@gmail.com>
2023-06-19 19:21:54 -0400

    Added function to show prompts and take prompt inputs

--- script.js
+++ script.js
@@ -22,7 +22,59 @@ function randNumbGenerator (max){
 }
 
 
+function getpasswordOptions(){
 
+  //prompt to ask how many characters the password should be
+  //parseInt used to convert input into number 
+  var passlength = parseInt(prompt ("How many characters would you like your password to contain?"), 10);
+  
+  //check if the input is a number
+  if (Number.isNaN(passlength)){
+    alert("Password length must be a number");
+    return null;
+  }
+  console.log(`result: ${passlength}`);
+  
+  //checks for number to be with 8 to 128
+  if (passlength < 8){
+    alert("Password must be atleast 8 characters")
+  }
+
+  if (passlength > 129){
+    alert("Password must be less than 129 characters")
+  }
+  
+
+var hasSpecialChar = confirm(
+  "Click OK if you would like to include special characters"
+);
+
+var hasNumericChar = confirm(
+  "Click OK if you would like to include numeric characters"
+)
+
+var haslowercaseChar = confirm(
+  "Click OK if you would like to include lowercase characters"
+)
+
+var hasUppercaseChar = confirm(
+  "Click OK if you would like to include uppercase characters"
+)
+
+//returns an object with password options 
+return {
+  passlength,
+  hasSpecialChar,
+  hasNumericChar,
+  haslowercaseChar,
+  hasUppercaseChar,
+}
+}
+
+//added the function to call prompts under the generatePassword function in the Assignment Code
+function generatePassword(){
+  var passwordOptions = getpasswordOptions();
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
